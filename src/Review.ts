4c3932fb47ebf4a3966b99cad67cc308e2ac1b90
CJ Salem <cj@specialblend.dev>
2025-01-02 14:16:13 -0600

    wip

--- src/Review.ts
+++ src/Review.ts
@@ -1,6 +1,7 @@
 import { Ollama } from "npm:ollama";
 import * as JSONSchema from "npm:jsonschema";
 import * as Yaml from "jsr:@std/yaml";
+import Jira2Md from "npm:jira2md";
 
 import { Issue } from "./Issue.ts";
 import { Store } from "./Store.ts";
@@ -191,4 +192,55 @@ export function ReviewService(
             storyPointsRange: [storyPoints * score, storyPoints],
         };
     }
+}
+
+async function publish(report: Report) {
+    const text = await format(report, { format: "jira" });
+    return await issueService.upsertComment(report.issue, text);
+}
+
+async function format(review: Review, options: { format: string }) {
+    const markdown = fmtReportMarkdown(review);
+    if (options.format === "markdown") return markdown;
+    return await Jira2Md.to_jira(markdown);
+}
+
+function normalizeEstimate(estimate: Estimate, review: Review) {
+    const normalizedConfidence = Math.round(
+        estimate.confidence * review.score,
+    );
+    return { ...estimate, normalizedConfidence };
+}
+
+function fmtStoryPointsRange(normalizedEstimate: NormalizedEstimate) {
+    const { storyPointsRange } = normalizedEstimate;
+    const [low, high] = storyPointsRange;
+    if (low === high) return low;
+    return `${low} - ${high}`;
+}
+
+function fmtReportMarkdown(review: Review) {
+    const score = `${Math.round(review.score * 100)}%`;
+    const checklist = review.checklist.map(
+        function fmtChecklistEntry({ entry, value }) {
+            if (value) return `{color:green}**✓**{color} ${entry.description}`;
+            return `{color:red}**✗**{color} ${entry.description}`;
+        },
+    );
+
+    return `
+
+#### ${review.issue.key}
+
+- ${checklist.join("\n- ")}
+
+\`\`\`markdown
+Refinement Score: ${score}
+Estimated Story Points: ${review.estimate.storyPoints}
+Confidence (Base / Normalized): ${review.estimate.confidence}% / ${review.normalizedEstimate.confidence}%
+Models: ${review.model} / ${review.estimate.model}
+\`\`\`
+
+*Generated with {color:red}♥{color} by [KAREN](https://github.com/specialblend/karen)*
+`;
 }
